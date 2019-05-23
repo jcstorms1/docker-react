14746923878982b7c559b6fae98af9d5a5e3ad29
Jordan Storms <jordan.storms@datadoghq.com>
2019-05-23 14:09:45 -0400

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          whoa dude 
+          I was changed on the feature branch woohoo!
         </p>
         <a
           className="App-link"
